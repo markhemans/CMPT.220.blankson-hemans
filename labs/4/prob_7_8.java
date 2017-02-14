ef7409c7be84e2b6e0269214255fb785df1e7426
Mark Blankson-Hemans <mark.blankson-hemans1@marist.edu>
2017-02-14 07:11:04 -0500

    Add files via upload

---
+++ labs/4/prob_7_8.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+/**
+ * * @author Mark Blankson-Hemans
+ * CMPT 220
+ * Juan Arias
+ * Problem (7.8)
+ */
+public class prob_7_8 {
+    public static void main (String[] args)
+    {
+    System.out.println("Enter Values: ");
+    double[] array1 = new double[10];
+    Scanner input = new Scanner(System.in);
+    for (int i=0;i<10 ;i++)
+           {
+    	   array1[i] = input.nextDouble();
+           }
+ System.out.println(average(array1));
+    }
+    
+    
+public static int average(int[] array)
+{
+    int sum=0;
+    int average;
+    int count =0;
+    for (int i=0;i<10 ;i++)
+           {
+    	   sum =  sum + array[i];
+           count++;
+           }
+    average = sum / count;
+    return average;
+}
+
+public static double average(double[] array)
+{
+   double sum=0;
+    double average;
+    int count =0;
+    for (int i=0;i<10 ;i++)
+           {
+    	   sum =  sum + array[i];
+           count++;
+           }
+    average = sum / count;
+    return average; 
+}
+
+}
