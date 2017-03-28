e950db7b3977892d1a06714a1813ed54b52d7634
Mark Blankson-Hemans <mark.blankson-hemans1@marist.edu>
2017-03-28 17:33:54 -0400

    Update prob_10_3.java

--- labs/7/prob_10_3.java
+++ labs/7/prob_10_3.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author mark
+ * * @author Mark Blankson-Hemans
+ * CMPT 220
+ * Juan Arias
+ * Problem (10.3)
  */
 public class prob_10_3
 {
