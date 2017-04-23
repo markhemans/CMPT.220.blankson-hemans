24e9cdcbfb91181d0afc37dd910cc771f0d16cca
Mark Blankson-Hemans <mark.blankson-hemans1@marist.edu>
2017-04-23 14:39:55 -0400

    Add files via upload

---
+++ labs/10/prob_13_3.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class prob_13_3 {
+     
+    public static void sort(ArrayList<Integer> list)
+     {
+         
+        for (int i = 0; i < list.size() - 1; i++)
+        {
+            int min = list.get(i);
+            int index = i;
+
+            for (int j = i + 1; j < list.size(); j++)
+            {
+                if (min > list.get(j))
+                {
+                    min = list.get(j);
+                    index = j;
+                }
+            }
+
+            if (index != i)
+            {
+                list.set(index, list.get(i));
+                list.set(i, min);
+            }
+        }
+
+    }
+}
+    
+
